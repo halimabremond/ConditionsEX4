8a03ef69e52729e9f64d5f2890379c8427f81939
halimabremond <halima.bremond@yahoo.com>
2021-02-18 15:00:34 +0100

    x4

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConditionsEX4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Login: ");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            string str2 = Console.ReadLine();
+
+            switch ((str1, str2))
+            {
+                case ("toto", "tata"): Console.WriteLine("Bienvenue à la Manu"); break;
+
+            }
+        }
+    }
+}
